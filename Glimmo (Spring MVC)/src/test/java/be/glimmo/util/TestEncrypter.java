086b574ebfac36b6e1a6b8c1ebf92fa9fbbfe3c8
Kar Hoo Yiu <superk888@gmail.com>
2012-05-03 16:18:11 +0200

    Adding test class for Encrypter

---
+++ Glimmo (Spring MVC)/src/test/java/be/glimmo/util/TestEncrypter.java
@@ -0,0 +1,51 @@
+package be.glimmo.util;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class TestEncrypter {
+	@DataProvider(name="invalidPasswordProvider")
+	public Object[][] createInvalidPassword(){
+		return new Object[][]{
+				new Object []{""},
+				new Object []{" "},
+				new Object []{"\n"},
+				new Object []{" \n "},
+				new Object []{"\t"},
+				new Object []{" \t "},
+				new Object []{"\r"},
+				new Object []{" \r "},
+				new Object[] {"  \n  \t  \r "}
+		};
+	}
+	
+	@DataProvider(name="validPasswordProvider")
+	public Object[][] createValidPassword(){
+		return new Object[][]{
+				new Object []{"pouet"},
+				new Object []{"test"},
+				new Object []{"12345"},
+				new Object []{"password"},
+				new Object []{"thisisfun"},
+				new Object[] {"trololol"}
+		};
+	}
+
+	/**
+	 *  Testing Encrypter behavior when it's passed different invalid password String objects to encrypt
+	 */
+	@Test(dataProvider="invalidPasswordProvider")
+	public void testEncryptionInvalidArgument(String pwd){
+		Assert.assertNull(Encrypter.encrypt(pwd));
+	}
+
+
+	/**
+	 *  Testing Encrypter behavior when it's passed different valid password String objects to encrypt
+	 */
+	@Test(dataProvider="validPasswordProvider")
+	public void testEncryptionValidArgument(String pwd){
+		Assert.assertNotNull(Encrypter.encrypt(pwd));
+	}
+}
