46bfada6c155c570adbe71532306f216d2c7899e
Kar Hoo Yiu <superk888@gmail.com>
2012-05-08 14:38:14 +0200

    Configured homepage mapping in HomeController

--- Glimmo (Spring MVC)/src/main/java/be/glimmo/controller/HomeController.java
+++ Glimmo (Spring MVC)/src/main/java/be/glimmo/controller/HomeController.java
@@ -1,8 +1,17 @@
 package be.glimmo.controller;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
+	
+	@RequestMapping(method=GET, value={"/", "/home"})
+	public String test(HttpSession session){
+		return "home";
+	}
 }
