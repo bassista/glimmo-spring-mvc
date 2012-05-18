28ca615feee80c674bd2d4be6d4cb4bfb0742a23
Kar Hoo Yiu <superk888@gmail.com>
2012-05-18 15:35:25 +0200

    Set up the

--- Glimmo (Spring MVC)/src/main/java/be/glimmo/controller/HomeController.java
+++ Glimmo (Spring MVC)/src/main/java/be/glimmo/controller/HomeController.java
@@ -2,17 +2,14 @@ package be.glimmo.controller;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller(value="HomeController")
 public class HomeController {
 	
-	@RequestMapping(method=GET, value={"/", "/home"})
-	public String test(HttpSession session){
-		System.out.println("Working on branch");
+	@RequestMapping(method=GET)
+	public String directToHomepage(){
 		return "home";
 	}
 }
