605bea9bc4fceab03658162311f4d837f3a6c1a6
Kar Hoo Yiu <superk888@gmail.com>
2012-05-24 23:52:58 +0200

    Controller before working on model

--- Glimmo (Spring MVC)/src/main/java/be/glimmo/controller/HomeController.java
+++ Glimmo (Spring MVC)/src/main/java/be/glimmo/controller/HomeController.java
@@ -3,13 +3,16 @@ package be.glimmo.controller;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller(value="HomeController")
 public class HomeController {
 	
 	@RequestMapping(method=GET)
-	public String directToHomepage(){
+	public String directToHomepage(Model model){
+		// TODO retrieve list of goods to display and stuff them into the model object
+		
 		return "home";
 	}
 }
