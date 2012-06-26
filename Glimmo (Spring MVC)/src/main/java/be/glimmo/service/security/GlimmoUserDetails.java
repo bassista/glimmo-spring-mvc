4a1982d839e924adb0766f7a2d7e123062eaf7d2
Kar Hoo Yiu <karhooyiu@gmail.com>
2012-06-26 16:34:26 +0200

    First version of authentication

--- Glimmo (Spring MVC)/src/main/java/be/glimmo/service/security/GlimmoUserDetails.java
+++ Glimmo (Spring MVC)/src/main/java/be/glimmo/service/security/GlimmoUserDetails.java
@@ -1,6 +1,8 @@
 package be.glimmo.service.security;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,39 +10,48 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class GlimmoUserDetails implements UserDetails{
 	private static final long serialVersionUID = 469391597485421166L;
 
+	private Set<GlimmoUserAuthority> authorities = new HashSet<GlimmoUserAuthority>(); 
+	private String password;
+	private String username;
+	
+	public GlimmoUserDetails(String username, String password, Set<GlimmoUserAuthority> authorities){
+		this.username = username;
+		this.password = password;
+		
+		if(authorities != null){
+			for(GlimmoUserAuthority authority : authorities){
+				this.authorities.add(authority);
+			}
+		}
+	}
+	
+	
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
+		return authorities;
 	}
 
 	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.password;
 	}
 
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.username;
 	}
 
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 }
--- Glimmo (Spring MVC)/src/main/java/be/glimmo/service/security/UserDetailsServiceImpl.java
+++ Glimmo (Spring MVC)/src/main/java/be/glimmo/service/security/UserDetailsServiceImpl.java
@@ -1,15 +1,40 @@
 package be.glimmo.service.security;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.HibernateException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import be.glimmo.dao.UserDao;
+import be.glimmo.domain.User;
 
 @Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService{
+	@Autowired
+	UserDao userDao;
+	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+		User user = null;
+		
+		try{
+			user = userDao.findUserByUsernameOrEmail(usernameOrEmail);
+		}catch(HibernateException e){
+			throw new UsernameNotFoundException(e.getMessage());
+		}
 
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return null;
+		Set<GlimmoUserAuthority> authorities = new HashSet<GlimmoUserAuthority>(1); 
+		authorities.add(new GlimmoUserAuthority(user.getGrade()));
+		GlimmoUserDetails gud = new GlimmoUserDetails(user.getUsername(), user.getPassword(), authorities);
+		
+		return gud;
 	}
 
 }
