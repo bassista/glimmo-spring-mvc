732a0408f2e17e1d4cf1d63ea2ebc1ac22bbd5dd
Kar Hoo Yiu <karhooyiu@gmail.com>
2012-06-26 09:36:17 +0200

    Changed enum persistence format from ORDINAL to STRING

--- Glimmo (Spring MVC)/src/main/java/be/glimmo/domain/Description.java
+++ Glimmo (Spring MVC)/src/main/java/be/glimmo/domain/Description.java
@@ -4,6 +4,8 @@ import java.sql.Clob;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,6 +24,7 @@ public class Description {
 	private long id;
 	
 	@Column(name="LANGUAGE", updatable=false, nullable=false)
+	@Enumerated(EnumType.STRING)
 	private Language language;
 	
 	@Column(name="DESCRIPTION", nullable=false, length=1000)
