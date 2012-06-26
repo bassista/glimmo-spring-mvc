243fdf8526d3f6881813a5fe2930a2ce274cc94d
Kar Hoo Yiu <karhooyiu@gmail.com>
2012-06-26 09:23:07 +0200

    Changed the enumerated type to be persisted

--- Glimmo (Spring MVC)/src/main/java/be/glimmo/domain/User.java
+++ Glimmo (Spring MVC)/src/main/java/be/glimmo/domain/User.java
@@ -7,6 +7,8 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -41,6 +43,7 @@ public class User {
 	private String password;
 	
 	@Column(nullable=false)
+	@Enumerated(EnumType.STRING)
 	private UserGrade grade;
 	
 	@Column(name="GRADE_ENDING", nullable=true)
